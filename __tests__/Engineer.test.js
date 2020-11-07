5c130f34789fef242700fb83891a9787e4a38dd2
Lyly Long <lylylong@gmail.com>
2020-11-06 23:19:19 -0500

    testing engineer js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require("../lib/Engineer");
+const Employee = require("../lib/Employee");
+
+// testing if expect(received).toBe(expected)
+// passed
+test("check if can setup github", () => {
+  const testGithub = "lylylong";
+  const engineer = new Engineer("Engineer 1", 1, "111@testing.com", testGithub);
+  expect(engineer.github).toBe(testGithub);
+});
+// passed
+test("get role as engineer", () => {
+  const role = "Engineer";
+  const engineer = new Engineer("Engineer 1", 1, "111@testing.com", "lylylong");
+  expect(engineer.getRole()).toBe(role);
+});
+// passed
+test("check if can setup getGithub function", () => {
+  const testGithub = "lylylong";
+  const engineer = new Engineer("Engineer 1", 1, "111@testing.com", testGithub);
+  expect(engineer.getGithub()).toBe(testGithub);
+});
