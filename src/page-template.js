7613fb5c13092ac3d349c0263847908baa3c5368
Lyly Long <lylylong@gmail.com>
2020-11-08 22:57:13 -0500

    prepare the video

--- src/page-template.js
+++ src/page-template.js
@@ -30,7 +30,7 @@ const generateTeamCards = (teamProfile) => {
         <ul class="list-group list-group-flush">
           <li class="list-group-item">ID: ${engineer.getId()}</li>
           <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-          <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+          <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
         </ul>
       </div>
     </div>
